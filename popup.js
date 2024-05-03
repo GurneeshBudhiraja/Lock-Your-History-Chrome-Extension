461083610b7711b6be435f09cdc944b3c56afc8b
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2024-05-03 15:04:19 -0400

    feature: current time of the playing video

--- popup.js
+++ popup.js
@@ -9,6 +9,31 @@
   bookmarkBtn.style.marginBottom = "3px";
   bookmarkBtn.title = "Bookmark this video";
   controls.prepend(bookmarkBtn);
-
+  bookmarkBtn.addEventListener("click", youtubeBookmarker);
   
+
+  function youtubeBookmarker (){
+    const time = document.getElementsByClassName("video-stream")[0].currentTime
+    const currentTime =  convertTime(time);
+    
+  }
+  
+  // Function to convert seconds to YouTube format
+  function convertTime(seconds){
+    let hours = Math.floor(seconds / 3600)
+    let minutes = Math.floor((seconds % 3600) / 60);
+    let remainingSeconds = Math.floor(seconds % 60);
+
+    // Format the time components with leading zeros if necessary
+    let formattedHours = hours.toString().padStart(2, '0');
+    let formattedMinutes = minutes.toString().padStart(2, '0');
+    let formattedSeconds = remainingSeconds.toString().padStart(2, '0');
+
+    // Concatenate the formatted components into the YouTube format
+    let formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    
+    return formattedTime;
+  }
+    
+
 })();
