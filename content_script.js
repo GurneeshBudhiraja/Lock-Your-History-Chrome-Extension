a5ded70358586ad19e5ccb0005fa1c15faae0c69
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2024-05-07 17:14:21 -0400

    feature: sync local storage and UI enhance

--- content_script.js
+++ content_script.js
@@ -1,21 +1,40 @@
-let currentVideo = "";
+let currentVideoURL = "";
+let currentVideoID = "";
+let bookmarkArray = [];
 const fontAwesomeCDN = `
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 `;
+
+
 
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
   const { type, videoURL } = obj;
 
   if (type === "NEW") {
-    currentVideo = videoURL;
-    console.log(currentVideo);
+    currentVideoURL = videoURL;
+    currentVideoID = videoURL.split("v=")[1];
+    console.log(currentVideoURL);
+    console.log(currentVideoID);
     addBookmarkButton();
   }
 });
 
-function addBookmarkButton() {
-  document.head.insertAdjacentHTML("beforeend", fontAwesomeCDN);
+if(!currentVideoURL){
+  currentVideoURL = window.location.href;
+  currentVideoID = currentVideoURL.includes("youtube.com/watch?v=") && currentVideoURL.split("v=")[1];
+  if (currentVideoID){
+    addBookmarkButton();
+  } else {
+    console.log("Not a youtube video");
+  }
+}
+  
+
+async function addBookmarkButton() {
+  bookmarkArray = await getLocalStorage();
+  console.log(bookmarkArray);
   const bookmarkBtn = document.createElement("button");
+  bookmarkBtn.className = "bookmark-btn";
   bookmarkBtn.innerHTML = `<i class="fa-solid fa-plus fa-xl" style="color: #74C0FC;"></i> `;
   bookmarkBtn.title = "Bookmark this video";
   bookmarkBtn.style.width = "40px";
@@ -25,16 +44,23 @@ function addBookmarkButton() {
   bookmarkBtn.style.cursor = "pointer";
   bookmarkBtn.style.position = "relative";
   bookmarkBtn.style.top = "-18px";
-  youtubeLeftControls = document.querySelector(".ytp-right-controls");
-  youtubeLeftControls.prepend(bookmarkBtn);
   bookmarkBtn.addEventListener("click", bookmarkBtnClickHandler);
+  document.head.insertAdjacentHTML("beforeend", fontAwesomeCDN);
+  youtubeControls = document.querySelector(".ytp-right-controls");
+  if(document.getElementsByClassName("bookmark-btn").length > 0){
+    return;
+  }
+  youtubeControls.prepend(bookmarkBtn);  
 }
+
 function bookmarkBtnClickHandler() {
-  console.log("Bookmark button clicked");
   const bookmarkTime = convertTime(
     document.querySelector(".video-stream").currentTime
   );
+  addLocalStorage(bookmarkTime);
+
 }
+
 function convertTime(time) {
   const hours = Math.floor(time / 3600)
     .toString()
@@ -47,3 +73,27 @@ function convertTime(time) {
     .padStart(2, "0");
   return `${hours}:${minutes}:${seconds}`;
 }
+
+async function addLocalStorage(bookmarkTime) {
+  const bookmark = {
+    videoID: currentVideoID,
+    videoURL: currentVideoURL,
+    bookmarkTime,
+  };
+
+  // getting the current bookmarks from the storage
+  bookmarkArray = await getLocalStorage();
+  chrome.storage.sync.set({
+    [currentVideoID]: JSON.stringify([...bookmarkArray,bookmark]),
+  })
+  console.log(bookmarkArray);
+  return;
+}
+
+function getLocalStorage()  {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get([currentVideoID], (result) => {
+      resolve(result[currentVideoID] ? JSON.parse(result[currentVideoID]) : []);
+    });
+  });
+};
--- popup.js
+++ popup.js
@@ -1,1 +1,77 @@
-console.log("popup.js loaded");
+let currentTabURL = "";
+let currentTabID = "";
+let bookmarkArray = [];
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const tab = await getCurrentTab();
+  if(tab.url && tab.url.includes("youtube.com/watch")){
+    currentTabURL = tab.url;
+    currentTabID = tab.url.split("v=")[1];
+    document.querySelector("#heading").textContent = "Bookmarks for the video";
+    const bookmarkArray = await getLocalStorage();    
+    if(bookmarkArray.length > 0){
+      displayBookmarks(bookmarkArray);
+    }
+  } else {
+    document.querySelector("#heading").textContent = "Not a youtube video!";
+    document.querySelector("#heading").style.textAlign = "center";
+    document.querySelector(".instructions").textContent = "Please open a youtube video to bookmark it.";
+  }
+})
+
+function displayBookmarks(bookmarkArray) {
+  const div = document.querySelector("#bookmarks");
+  div.innerHTML = ""; // Clear existing content
+  console.log(bookmarkArray);
+  bookmarkArray.forEach((bookmark) => {
+    const bookmarkDiv = document.createElement("div");
+    bookmarkDiv.classList.add(`bookmark-${bookmark.bookmarkTime}`,"bookmark");
+
+    const trashIcon = document.createElement("i");
+    trashIcon.classList.add("fa-solid", "fa-trash");
+    trashIcon.style.color = "#FF6347";
+    trashIcon.setAttribute("aria-hidden", "true");
+    trashIcon.addEventListener('click',trashIconClickHandler);    
+
+    const bookmarkText = document.createElement("span");
+    bookmarkText.textContent = ` Bookmark at ${bookmark.bookmarkTime}`;
+
+    bookmarkDiv.appendChild(bookmarkText);
+    bookmarkDiv.appendChild(trashIcon);
+
+    div.appendChild(bookmarkDiv);
+  });
+}
+function trashIconClickHandler(e) {
+  const parentDiv = e.target.parentElement;
+  const bookmarkTime = parentDiv.querySelector("span").innerText.split(" ")[2];
+  parentDiv.remove();
+  console.log(bookmarkArray);
+  // Filter the bookmarkArray to remove the bookmark with the specified bookmarkTime
+  bookmarkArray.map((bookmark)=>{
+    console.log(bookmark);
+  })
+  console.log(bookmarkArray);
+  
+  // Save the updated bookmarkArray to Chrome storage
+  // chrome.storage.sync.set({ [currentTabID]: JSON.stringify(bookmarkArray) });
+  
+  // Update the displayed bookmarks
+  // displayBookmarks(bookmarkArray);
+}
+
+
+function getLocalStorage()  {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get([currentTabID], (result) => {
+      resolve(result[currentTabID] ? JSON.parse(result[currentTabID]) : []);
+    });
+  });
+};
